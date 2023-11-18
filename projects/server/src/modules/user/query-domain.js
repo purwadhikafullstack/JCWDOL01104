502a5925de35fd3a0545a356a3938911b038c56b
Muchamad Choirul Anwar <mcnwr76@Muchamads-MacBook-Air.local>
2023-11-18 11:32:04 +0700

    [FPMGJ-14] Feat: User Login

--- projects/server/src/modules/user/query-domain.js
+++ projects/server/src/modules/user/query-domain.js
@@ -1,13 +1,13 @@
 import User from "./repositories.js";
 import Role from "../../models/role.js";
+import AppError from "../../utils/app-error.js";
 
 export default class QueryUser {
   constructor() {
     this.user = new User();
   }
   async getUsers(query) {
-    // const params = { include: [{ model: Role }] };
-    const params = {};
+    const params = { include: [{ model: Role }] };
     const data = await this.user.findAllUser(params);
     return data;
   }
@@ -19,8 +19,22 @@ export default class QueryUser {
   }
 
   async getUserByEmail(email) {
-    const params = { where: { email: email } };
+    const params = { include: [{ model: Role }], where: { email: email } };
     const data = await this.user.findOneUser(params);
+    return data;
+  }
+
+  async getUserByPhoneNumber(phone) {
+    const params = { include: [{ model: Role }], where: { phone_number: phone } };
+    const data = await this.user.findOneUser(params);
+    return data;
+  }
+
+  async getUserByEmailOrPhoneNumber(emailOrPhoneNumber) {
+    const chekEmail = await this.getUserByEmail(emailOrPhoneNumber);
+    const chekPhone = await this.getUserByEmail(emailOrPhoneNumber);
+    if (!chekEmail && !chekPhone) throw new AppError("Email atau password tidak sesui", 403);
+    const data = chekEmail ? chekEmail.dataValues : chekPhone.dataValues;
     return data;
   }
 }
