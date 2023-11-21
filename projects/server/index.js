941b2e7ff5570c5730baaa1672baa270c9d5c368
danielgitgit <daniel.work.idn@gmail.com>
2023-11-21 20:11:09 +0700

    [FPMGJ-37] Feat : Tenant Propsnrooms

--- projects/server/index.js
+++ projects/server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import helmet from "helmet";
+import propertyRoutes from "../server/src/routers/property.router.js"
 
 import { connetionMysql } from "./src/config/db.js";
 
@@ -15,5 +16,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
+
+
+app.use ("/api/propertyList",propertyRoutes);
+// app.use("/api/productListData", productRoutes);
+
+
+
+console.log("helo")
 
 app.listen(PORT, () => console.log(`Server is Running on PORT:${PORT}`));
