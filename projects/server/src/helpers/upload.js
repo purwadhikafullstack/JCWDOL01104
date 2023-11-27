0add7287f6c015b6929da7e9212b6b3b75c2ddc7
mcnwr76 <62655869+AnwarAan@users.noreply.github.com>
2023-11-27 19:04:51 +0700

    [FPMGJ-15] Feat: Change Password (#10)
    * setup be
    
    * [FPMGJ-12] Feat: User Register
    
    * [FPMGJ-12] Feat: User Register
    
    * [FPMGJ-12] Feat: User Register
    
    * [FPMGJ-14] Feat: User Login
    
    * [FPMGJ-13] Feat: Verify Email
    
    * rest stagging are
    
    * resolve bug
    
    * resolve bug
    
    * [FPMGJ-15] Feat: Change Password
    
    * [FPMGJ-16] Feat: Reset Password
    
    * [FPMGJ-18] Feat: Edit User
    
    * main
    
    * resolve bug
    
    ---------
    
    Co-authored-by: Muchamad Choirul Anwar <mcnwr76@Muchamads-MacBook-Air.local>

---
+++ projects/server/src/helpers/upload.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `images/${file.fieldname}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.split("/") === "jpg" || "jpeg" || "png") {
+    cb(null, true);
+  } else {
+    cb(new Error("extention false"), false);
+  }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter, limits: { fieldSize: 1e6 } }).single("file");
+export default upload;
